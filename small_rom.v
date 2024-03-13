0bd8617ea72e4e0b37fc5e5dd689bbc1a763c1a6
vladimir-vasyuk <vasyuk.sao@gmail.com>
2024-03-13 11:32:48 +0300

    Added comments

--- small_rom.v
+++ small_rom.v
@@ -1,5 +1,10 @@
+//=================================================================================
+// Реализация контроллера Ethernet на основе приемо-передатчика RTL8211EG
+//---------------------------------------------------------------------------------
+// Модуль MAC адреса
+//=================================================================================
 module small_rom
-#(parameter ADDR_WIDTH = 3, parameter WIDTH = 8, parameter FILE = "sarom.txt")
+#(parameter ADDR_WIDTH = 3, parameter WIDTH = 8, parameter FILE = "../../hdl/delqap/sarom.txt")
 (
    input                      clk,
 // input [ADDR_WIDTH - 1:0]   addr,
