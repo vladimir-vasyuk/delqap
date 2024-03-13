f7fd71eaae4b11d7c0a81e901b8c362de0f8f814
vladimir-vasyuk <vasyuk.sao@gmail.com>
2024-03-13 11:15:57 +0300

    Added comments

--- bdl.v
+++ bdl.v
@@ -1,36 +1,28 @@
 //=================================================================================
 // Реализация контроллера Ethernet DELQA на основе процессора М4 (LSI-11M)
-//
-// 
-//
 //=================================================================================
 // Модуль BDL
 //=================================================================================
 module bdl(
-// Bus
-	input				wb_clk_i,
-	input  [2:0]	wb_adr_i,
-	input  [15:0]	wb_dat_i,
-	output [15:0]	wb_dat_o,
-	input				wb_cyc_i,
-	input				wb_we_i,
-	input  [1:0]	wb_sel_i,
-	input				wb_stb_i,
-	output			wb_ack_o,
-// DMA
-	input  [2:0]	dma_adr_i,
-	input  [15:0]	dma_dat_i,
-	output [15:0]	dma_dat_o,
-	input				dma_we_i,
-	input				dma_stb_i
+// Внутренняя шина 
+	input				wb_clk_i,	// тактовая частота шины
+	input  [2:0]	wb_adr_i,	// адрес
+	input  [15:0]	wb_dat_i,	// входные данные
+	input				wb_cyc_i,	// начало цикла шины
+	input				wb_we_i,		// разрешение записи (0 - чтение)
+	input  [1:0]	wb_sel_i,	// выбор байтов для записи 
+	input				wb_stb_i,	// строб цикла шины
+	output			wb_ack_o,	// подтверждение выбора устройства
+// Шина ПДП/DMA
+	input  [2:0]	dma_adr_i,	// адрес
+	input  [15:0]	dma_dat_i,	// входные данные
+	input				dma_we_i,	// разрешение записи (0 - чтение)
+	input				dma_stb_i,	// строб
+// Общий выход данных
+	output [15:0]	bdl_dat_o
 );
 
-// Сигналы упраления обменом с шиной
-//wire			bus_strobe, bus_read_req, bus_write_req;
-//assign bus_strobe = wb_cyc_i & wb_stb_i & ~wb_ack_o;	// строб цикла шины
-//assign bus_read_req = bus_strobe & ~wb_we_i;				// запрос чтения
-//assign bus_write_req = bus_strobe & wb_we_i;				// запрос записи
-
+// Формирование сигнала подтверждения выбора устройства
 reg			ack;
 always @(posedge wb_clk_i)
    if (wb_stb_i & wb_cyc_i)
@@ -45,10 +37,9 @@ wire [2:0]	bdladr;			// адрес BDL
 wire			bdlwe;			// сигнал записи BDL
 
 assign bdlwe = dma_stb_i? dma_we_i : (wb_stb_i? wb_we_i : 1'b0);
-assign bdladr = dma_stb_i? dma_adr_i[2:0] : (wb_stb_i? wb_adr_i[2:0] : 3'b0);
+assign bdladr = dma_stb_i? dma_adr_i[2:0] : wb_adr_i[2:0];
 assign bdldin = dma_stb_i? dma_dat_i : wb_dat_i;
-assign dma_dat_o = dma_stb_i? bdldout : 16'b0;
-assign wb_dat_o = bdldout;
+assign bdl_dat_o = bdldout;
 
 regf #(.NUM(6)) bdl(
    .clk(wb_clk_i),
--- bufmem.v
+++ bufmem.v
@@ -1,7 +1,6 @@
 //=================================================================================
 // Реализация контроллера Ethernet DELQA на основе процессора М4 (LSI-11M)
-//
-// 
+// Всевозможные модули памяти
 //
 //=================================================================================
 // Модуль регистровой памяти
@@ -89,16 +88,6 @@ module rxbuf(
 	input  [1:0]	adr_mode_i	// режим работы (шина, DMA, Ethernet)
 );
 
-// Блок памяти
-reg  [10:0]	adr_i;	// адрес
-reg  [15:0]	dat_i;	// входные данные
-reg			clk_i;	// тактовая
-reg			we_i;		// разрешение записи
-wire [15:0]	dat_o;	// выходные данные
-
-assign wb_dat_o = dat_o;
-assign dma_dat_o = dat_o;
-
 // Сигналы упраления обменом с шиной
 wire bus_strobe = wb_cyc_i & wb_stb_i & ~wb_ack_o;	// строб цикла шины
 wire bus_we = bus_strobe & wb_we_i;						// запрос записи
@@ -110,9 +99,18 @@ always @(posedge wb_clk_i)
 		ack <= 1'b1;
    else
 		ack <= 1'b0;
-assign wb_ack_o = ack & wb_stb_i;    
+assign wb_ack_o = ack & wb_stb_i;
+
+wire [15:0]	dat_o;	// выходные данные
+assign wb_dat_o = dat_o;
+assign dma_dat_o = dat_o;
 
 // Мультиплексер адреса, данных и управляющих сигналов
+reg  [10:0]	adr_i;	// адрес
+reg  [15:0]	dat_i;	// входные данные
+reg			clk_i;	// тактовая
+reg			we_i;		// разрешение записи
+
 always @(*) begin
 	case (adr_mode_i)
 		2'b00: begin	// режим щины
@@ -174,19 +172,9 @@ module txbuf(
 	input	 [9:0]	eth_adr_i,	// адрес
 	output [15:0]	eth_dat_o,	// выходные данные
 	input				eth_clk_i,	// тактовая частота
-
+//
 	input  [1:0]	adr_mode_i	// режим работы (шина, DMA, Ethernet)
 );
-
-// Блок памяти
-reg  [9:0]	adr_i;	// адрес
-reg  [15:0]	dat_i;	// входные данные
-reg			clk_i;	// тактовая
-reg			we_i;		// разрешение записи
-wire [15:0]	dat_o;	// выходные данные
-
-assign wb_dat_o = dat_o;
-assign eth_dat_o = dat_o;
 
 // Сигналы упраления обменом с шиной
 wire bus_strobe = wb_cyc_i & wb_stb_i & ~wb_ack_o;	// строб цикла шины
@@ -201,10 +189,18 @@ always @(posedge wb_clk_i)
 		ack <= 1'b0;
 assign wb_ack_o = ack & wb_stb_i;
 
-// Сигнал разрешения записи по каналу ПДП/DMA
-wire dma_we = dma_stb_i & dma_we_i;
+wire [15:0]	dat_o;	// выходные данные
+wire			dma_we;
+assign dma_we = dma_stb_i & dma_we_i;
+assign wb_dat_o = dat_o;
+assign eth_dat_o = dat_o;
 
 // Мультиплексер адреса, данных и управляющих сигналов
+reg  [9:0]	adr_i;	// адрес
+reg  [15:0]	dat_i;	// входные данные
+reg			clk_i;	// тактовая
+reg			we_i;		// разрешение записи
+
 always @(*) begin
 	case (adr_mode_i)
 		2'b00: begin	// режим щины
@@ -249,16 +245,16 @@ endmodule
 // Модуль RAM с управляющей программой + BD ROM
 //=================================================================================
 module firmware (
-   input          wb_clk_i,
-   input  [15:0]  wb_adr_i,
-   input  [15:0]  wb_dat_i,
-   output [15:0]  wb_dat_o,
-   input          wb_cyc_i,
-   input          wb_we_i,
-   input  [1:0]   wb_sel_i,
-   input          prg_stb_i,
-	input          rom_stb_i,
-   output         wb_ack_o
+   input          wb_clk_i,	// тактовая частота шины
+   input  [15:0]  wb_adr_i,	// адрес
+   input  [15:0]  wb_dat_i,	// входные данные
+   output [15:0]  wb_dat_o,	// выходные данные
+   input          wb_cyc_i,	// начало цикла шины
+   input          wb_we_i,		// разрешение записи (0 - чтение)
+   input  [1:0]   wb_sel_i,	// выбор байтов для записи 
+   input          prg_stb_i,	// строб модуля RAM
+	input          rom_stb_i,	// строб модуля BD ROM
+   output         wb_ack_o		// подтверждение выбора устройства
 );
 
 wire [1:0]	enaprg;
