14372a323e603cbacdfd8d60615c325e07f92a30
vladimir-vasyuk <vasyuk.sao@gmail.com>
2024-04-24 17:12:47 +0300

    Changed satiny timer logic

--- santim.v
+++ santim.v
@@ -6,28 +6,27 @@
 //=================================================================================
 module santim(
 	input			clock_i,		// 2,5 MHz тактовая
-	input			ena_i,		// Разрешение работы
 	input			gen_i,		// Сигнал генерации
 	output		out_o			// Выход, активный 1
 );
 
-reg			nbdcok;
-reg  [3:0]	bdcok_cnt;
+reg			rbdcok = 1'b0;
+reg  [3:0]	bdcok_cnt = 4'b0;
 localparam	BDCOK_LIMIT = 10;
-assign out_o = ~nbdcok;
+assign out_o = rbdcok;
 
 // Генерация отрицательного BDCOK ~4 msec.
 always @(posedge clock_i) begin
-	if(ena_i & gen_i) begin
+	if(gen_i) begin
 		if(bdcok_cnt != BDCOK_LIMIT) begin
 			bdcok_cnt <= bdcok_cnt + 1'b1;
-			nbdcok <= 1'b0;
+			rbdcok <= 1'b1;
 		end
 		else
-			nbdcok <= 1'b1;
+			rbdcok <= 1'b0;
 	end
 	else begin
-		nbdcok <= 1'b1; bdcok_cnt <= 4'b0;
+		rbdcok <= 1'b0; bdcok_cnt <= 4'b0;
 	end
 end
 
